dc3baa40316a3bb91af53ad6b4aff466fb140f2e
kassemch1 <kassemchamseddine1@gmail.com>
2024-03-31 16:51:22 +0300

    database create

---
+++ database/migrations/2024_03_31_131759_create_shopping_cart_items_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('shopping_cart_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('cart_id')->references('id')->on('shopping_carts')->onDelete('cascade');
+            $table->foreignId('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->integer('quantity');
+            $table->double('unit_price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('shopping_cart_items');
+    }
+};
