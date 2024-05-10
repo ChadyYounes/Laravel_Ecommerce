d577b1f6f04df14a6fd7c51a228433436ec6b004
ChadyYounes <chadycs999@gmail.com>
2024-05-10 15:47:53 +0300

    added products cards for each stores + add to shopping card button and contact seller button

--- resources/views/buyerLayout/storeProducts.blade.php
+++ resources/views/buyerLayout/storeProducts.blade.php
@@ -6,27 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bidding Events</title>
 
-    <link href="{{asset("css/navbar.css")}}" rel="stylesheet">
+    
+    <link rel="stylesheet" type="text/css" href="{{asset("assets/css/productCardStyle.css")}}">
 
-    <link href="{{asset("css/logout.css")}}" rel="stylesheet">
-    <script src="https://kit.fontawesome.com/9055df38da.js" crossorigin="anonymous"></script>
-
-    <!-- Template Stylesheet -->
-    <link href="{{asset("css/style.css")}}" rel="stylesheet">
-
-    <!-- favicon -->
-    <link rel="shortcut icon" type="image/png" href="{{asset('assets/img/favicon.png')}}">
-    <!-- google font -->
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap" rel="stylesheet">
-    <!-- fontawesome -->
-    <link rel="stylesheet" href="{{asset('assets/css/all.min.css')}}">
-    <!-- bootstrap -->
-    <link rel="stylesheet" href="{{asset('assets/bootstrap/css/bootstrap.min.css')}}">
-
-    <link rel="stylesheet" href="{{asset('assets/css/main.css')}}">
-    <!-- responsive -->
-    <link rel="stylesheet" href="{{asset('assets/css/responsive.css')}}">
     <style>
 
         #logout-div {
@@ -117,7 +99,252 @@
             color:    #1e90ff;
         }
         
+        body{
 
+background-color: #eee;
+}
+.container{
+width: 900px;
+}
+
+.card{
+
+background-color: #fff;
+border:none;
+border-radius: 10px;
+width: 190px;
+box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+
+}
+.image-container {
+    width: 100%; /* Set width to 100% for responsiveness */
+    height: 200px; /* Set a fixed height for the container */
+    overflow: hidden; /* Hide overflow to prevent stretching */
+    border-radius: 10px; /* Apply border radius for rounded corners */
+}
+
+.thumbnail-image {
+    width: 100%; /* Set width to 100% to fill the container */
+    height: auto; /* Allow image height to adjust based on aspect ratio */
+    object-fit: cover; /* Ensure image covers its container */
+}
+
+
+
+.discount{
+
+background-color: red;
+padding-top: 1px;
+padding-bottom: 1px;
+padding-left: 4px;
+padding-right: 4px;
+font-size: 10px;
+border-radius: 6px;
+color: #fff;
+}
+
+.wishlist{
+
+height: 25px;
+width: 25px;
+background-color: #eee;
+display: flex;
+justify-content: center;
+align-items: center;
+border-radius: 50%;
+box-shadow:  0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+}
+
+.first{
+
+position: absolute;
+width: 100%;
+    padding: 9px;
+}
+
+
+.dress-name{
+font-size: 13px;
+font-weight: bold;
+width: 75%;
+}
+.dress-description {
+    font-size: 11px; /* Adjust the font size as needed */
+    color: #555; /* Adjust the color to make it a bit less dark */
+}
+
+
+.new-price{
+font-size: 13px;
+font-weight: bold;
+color: red;
+
+}
+.old-price{
+font-size: 8px;
+font-weight: bold;
+color: grey;
+}
+
+
+.btn{
+width: 14px;
+height: 14px;
+border-radius: 50%;
+padding: 3px;
+}
+
+.creme{
+background-color: #fff;
+border: 2px solid grey;
+
+
+}
+.creme:hover {
+border: 3px solid grey;
+}
+
+.creme:focus {
+background-color: grey;
+
+}
+
+
+.red{
+background-color: #fff;
+border: 2px solid red;
+
+}
+
+
+.red:hover {
+border: 3px solid red;
+}
+.red:focus {
+background-color: red;
+}
+
+
+
+.blue{
+background-color: #fff;
+border: 2px solid #40C4FF;
+}
+
+.blue:hover {
+border: 3px solid #40C4FF;
+}
+.blue:focus {
+background-color: #40C4FF;
+}
+.darkblue{
+background-color: #fff;
+border: 2px solid #01579B;
+}
+.darkblue:hover {
+border: 3px solid #01579B;
+}
+.darkblue:focus {
+background-color: #01579B;
+}
+.yellow{
+background-color: #fff;
+border: 2px solid #FFCA28;
+}
+.yellow:hover {
+border-radius: 3px solid #FFCA28;
+}
+.yellow:focus {
+background-color: #FFCA28;
+}
+
+
+.item-size{
+width: 15px;
+height: 15px;
+border-radius: 50%;
+background: #fff;
+border: 1px solid grey;
+color: grey;
+font-size: 10px;
+text-align: center;
+align-items: center;
+display: flex;
+justify-content: center;
+}
+
+
+.rating-star{
+font-size: 10px !important;
+}
+.rating-number{
+font-size: 10px;
+color: grey;
+
+}
+.buy{
+font-size: 12px;
+color: purple;
+font-weight: 500;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+.voutchers{
+background-color: #fff;
+border:none;
+border-radius: 10px;
+width: 190px;
+box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+overflow: hidden;
+
+}
+.voutcher-divider{
+
+display: flex;
+
+}
+.voutcher-left{
+width: 60%
+}
+.voutcher-name{
+color: grey;
+font-size: 9px;
+font-weight: 500;
+}
+.voutcher-code{
+color: red;
+font-size: 11px;
+font-weight: bold;
+}
+.voutcher-right{
+width: 40%;	 
+background-color: purple;
+color: #fff;
+}
+
+.discount-percent{
+font-size: 12px;
+font-weight: bold;
+position: relative;
+top: 5px;
+}
+.off{
+font-size: 14px;
+position: relative;
+bottom: 5px;
+}
 
     </style>
 </head>
@@ -126,12 +353,102 @@
 @include('buyerLayout.buyerNav')
 
 
-    
-@foreach($product as $p)
+@php
+    $chunks = $product->chunk(3);
+@endphp
 
-  
+<div class="pricing4 py-5 bg-light">
+  <div class="container">
+    <!-- Row  -->
+    <div class="row justify-content-center">
+      <div class="col-md-8 text-center">
+        <h3 class="mb-3"><span class="orange-text">Extraordinary</span> Pricing for your Daily Use</h3>
+        <h6 class="subtitle font-weight-normal">You can rely on our amazing features list and also our customer services will be a great experience for you without a doubt</h6>
+      </div>
+    </div>
+    <!-- Row  -->
+    @foreach($chunks as $chunk)
+    <div class="row mt-4">
+        @foreach($chunk as $p)
+        
+        <div class="card">
+
+            <div class="image-container">
+
+                <div class="first">
+                    
+                    <div class="d-flex justify-content-between align-items-center">
+                    <span class="discount">-25%</span>
+                    <span class="wishlist"><ion-icon name="heart-outline"></ion-icon></span>
+                    </div>
+                </div>
+
+                <div class="image-container">
+    <img src="{{ asset($p->product_url) }}" class="img-fluid rounded thumbnail-image">
+</div>
+
     
-@endforeach
+
+        </div>
+
+
+        <div class="product-detail-container p-2">
+            <div class="d-flex justify-content-between align-items-center">
+                    <h5 class="dress-name">{{$p->product_name}}</h5>
+                    
+                    <div class="d-flex flex-column mb-2">
+
+                        <span class="new-price">{{$p->price}}$</span>
+                        <small class="old-price text-right"></small>
+                    </div>
+
+            </div>
+
+            <h6 class="dress-description">{{$p->description}}</h6>
+        <div class="d-flex justify-content-between align-items-center pt-1">
+
+       
+
+            
+            
+
+        </div>
+
+            <!-- Contact and shoppingg cartttttt-->
+        <div class="d-flex justify-content-between align-items-center pt-1">
+        <div class="d-flex justify-content-center">
+                <a href="" class="d-flex align-items-center">
+            <ion-icon name="call"></ion-icon>
+                <span class="rating-number">Contact</span>
+                </a>
+            </div>
+
+            <div class="d-flex justify-content-center">
+                <a href="" class="d-flex align-items-center">
+                    <span class="buy">Add To Cart</span>
+                    <ion-icon name="cart"></ion-icon>
+                </a>
+        </div>
+
+            
+            
+
+            
+        </div>
+
+    
+
+</div>
+
+</div>
+
+        @endforeach
+    </div>
+    @endforeach
+  </div> <!-- Close container -->
+</div> <!-- Close pricing4 -->
+
+
 
 
 
